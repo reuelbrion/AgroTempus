2b352dc4cf1daecb5745f262a09c1a46fb20f6c0
Reuel <reuelbrion@gmail.com>
2015-05-13 12:42:20 +0200

    zeroconf lib
    app - cordova

--- app_cordova/platforms/firefoxos/www/cordova_plugins.js
+++ app_cordova/platforms/firefoxos/www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.triggertrap.ZeroConf/www/ZeroConf.js",
+        "id": "com.triggertrap.ZeroConf.zeroconf",
+        "clobbers": [
+            "ZeroConf"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.triggertrap.ZeroConf": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
